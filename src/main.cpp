07a4c50a9333c1f2af5d59630719040751e0c710
Hégron Stanislas <Zargith@localhost.localdomain>
2019-09-16 17:02:45 +0200

    Add intranet example and it works!

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+//
+// EPITECH PROJECT, 2019
+// babel
+// File description:
+//
+//
 
-int main()
+#include <stdio.h>
+#include <opus/opus.h>
+#include <portaudio.h>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QPushButton>
+#include <QApplication>
+
+int main(int argc, char **argv)
 {
-    std::cout << "Hello, World!" << std::endl;
-    return EXIT_FAILURE;
+    QApplication app (argc, argv);
+    QPushButton button ("Hello world !");
+
+    button.show();
+    return app.exec();
 }
