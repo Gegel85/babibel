755cae0580dde77571b614453f7af092e445ecef
Andgel <andgel.halley@epitech.eu>
2019-09-25 11:34:44 +0200

    Added dummy main

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -5,77 +5,48 @@
 ** main.cpp
 */
 
-#include <stdio.h>
-#include <opus/opus.h>
-#include <portaudio.h>
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QPushButton>
-#include <QtWidgets/QLCDNumber>
-#include <QtWidgets/QSlider>
+#include <iostream>
+#include <thread>
+#include "graphical/GUIScreen.hpp"
 
-class Window : public QWidget
+namespace Babel
 {
-    public:
-        Window() : QWidget()
-        {
-            this->setFixedSize(300, 150);
+	class TempWindow : public GUIScreen {
+	private:
+		Vector2<unsigned int> _size;
 
-            this->_button = new QPushButton("Quitter", this);
+	public:
+		TempWindow(Vector2<unsigned int> size) : GUIScreen(), _size{size} {};
 
-            this->_button->setFont(QFont("Comic Sans MS", 14));
-            this->_button->setCursor(Qt::PointingHandCursor);
-            this->_button->setIcon(QIcon("./assets/images/02_grin.png"));
-            this->_button->move(110, 50);
-            setFixedSize(300, 150);
+		Vector2<unsigned int> getSize() const override
+		{
+			return this->_size;
+		};
 
-            // Connexion du clic du bouton à la fermeture de l'application
-            QObject::connect(this->_button, SIGNAL(clicked()), qApp, SLOT(quit()));
-            this->_lcd = new QLCDNumber(this);
-            this->_lcd->setSegmentStyle(QLCDNumber::Flat);
-            this->_lcd->move(50, 20);
+		virtual void setFullscreen(bool fullscreen = true) override
+		{
+			std::cout << "Fullscreen is now " << (fullscreen ? "on" : "off") << std::endl;
+		};
 
-            this->_slider = new QSlider(Qt::Horizontal, this);
-            this->_slider->setGeometry(10, 90, 150, 20);
-            QObject::connect(this->_slider, SIGNAL(valueChanged(int)), this->_lcd, SLOT(display(int)));
-        };
-        ~Window() {
-            if (this->_button)
-                delete (this->_button);
-        };
+		virtual void setSize(Vector2<unsigned int> size)
+		{
+			std::cout << "New window size (" << size.x << ", " << size.y << ")" << std::endl;
+		};
 
-    private:
-        QPushButton *_button;
-        QLCDNumber *_lcd;
-        QSlider *_slider;
-    };
+		virtual bool refresh()
+		{
+			std::cout << "Rendering objects" << std::endl;
+			std::this_thread::sleep_for(std::chrono::milliseconds(17));
+			return true;
+		};
+	};
+}
 
 int main(int argc, char **argv)
 {
-/*
-    QApplication app (argc, argv);
-    QWidget window;
-    window.setFixedSize(300, 150);
+	Babel::TempWindow	window{{640, 480}};
 
-    QPushButton button ("Hello world !", &window);
-    button.setFont(QFont("Comic Sans MS", 25));
-    button.setToolTip("C'est un bouton magique! *clin d'oeil*");
-    button.setIcon(QIcon("./assets/images/02_grin.png"));
-    button.setCursor(Qt::PointingHandCursor);
-    button.setGeometry(10, 50, 180, 70);
-
-    QPushButton otherButton("Autre bouton", &button);
-    otherButton.move(50, 15);
-    otherButton.setFont(QFont("", 11));
-
-    window.show();
-    return app.exec();
-*/
-
-    QApplication app(argc, argv);
-
-    Window myWindow;
-    myWindow.show();
-
-    return app.exec();
-
+	do {
+		//Some stuff
+	} while (window.refresh());
 }
