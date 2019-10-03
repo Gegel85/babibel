07642d39c357a1e2135dff93f93252b1a4e18d97
iElden <mceldenn@gmail.com>
2019-10-03 11:43:56 +0200

    Added docstring in network/Socket and network/Protocol

--- src/network/Protocol.hpp
+++ src/network/Protocol.hpp
@@ -16,35 +16,76 @@
 
 #define VERSION_STR std::string{'\0', '\x01'}
 
+
+
 namespace Babel::Protocol
 {
+    //! @brief Code réprésentat l'opération que le client/serveur veut effectuer.
+    //! @note Une taille `d` indique que la taille est dynamique et doit être trouvé dans le header du Packet.
+    //! @warning Si la taille attendu ne correspond pas à la taille donnée dans le header, une erreur BAD_PACKET sera levée.
 	enum Opcode {
-		OK,
-		KO,
-		HELLO,
-		BYE,
+        //! taille: d | Code générique en cas de succès.
+        OK,
+        //! taille: d | Code générique en cas d'erreur.
+        KO,
+        //! taille: 2 | Initialisation du socket, envoie en data la version du protocole.
+        HELLO,
+        //! taille: 4 | Fermeture du socket, envoie en data le code retour. (voir: ErrorReason )
+        BYE,
+        //! taille: 64| (Client) Connecte le client, envoie en data le nom d'utilisateur et le mot de passe de longueur 32 chacun. (Rempli\s avec des bytes nuls)
 		LOGIN,
+		//! taille: 0 | (Client) Déconecte le client.
 		LOGOUT,
+        //! taille: 64| (Client) Enregistre le client. Même data que pour le REGISTER.
 		REGISTER,
+        //! taille: d | (Client) Demande la liste d'amis au serveur, envoie une liste d'ID de chacun 4 octets.
 		GET_FRIENDS,
+		//! taille: d | (Client) Renvoie une structure de donnée `User`.
+        //! @code
+        //!   struct User {
+        //!       unsigned int id;
+        //!       char username[32];
+        //!   };
+        //! @endcode
 		GET_USER_INFOS,
+		//! taille: 4 | Envoie au (ou reçoit du) serveur une demande d'appel avec l'ID de l'utilisateur que le client souhaite contacter.
 		CALL,
+		//! taille: 6 | Appel accepté. Renvoie une structure `Call`.
+        //! @code
+        //!   struct Call {
+        //!       unsigned int ip_adress;
+        //!       unsigned short port;
+        //!   };
+        //! @endcode
 		CALL_REFUSED,
+		//! taille: 0 | Appel refusé.
 		CALL_ACCEPTED,
 	};
 
 	namespace ErrorReason {
+	    //! Code renvoyé en cas de fermeture normale de la connection.
 		extern std::string NORMAL_CLOSURE;
+		//! Code renvoyé quand le format du paquet reçu ne correspond pas à celui qui était attendu.
 		extern std::string BAD_PACKET;
+		//! Code renvoyé quand la version du protocol du client ne correspond pas à celui du serveur.
 		extern std::string BAD_VERSION;
+		//! Code renvoyé quand l'`Opcode` reçu ne correspond à aucun dans l'enum.
 		extern std::string BAD_OPCODE;
+		//! Le nom d'utilisateur ou le mot de passe est incorrect.
 		extern std::string BAD_CREDENTIALS;
+		//! L'utilisateur est déjà connecté.
 		extern std::string ALREADY_CONNECTED;
+		//! La ressource est déjà utilisé par un autre client.
 		extern std::string ALREADY_USED;
+		//! Le client a déjà lancé un appel.
 		extern std::string ALREADY_CALLING;
+		//! Le client n'est pas authentifié.
 		extern std::string NOT_CONNECTED;
+		//! Le client n'a pas l'autorisation d'accéder à la ressource demandée.
 		extern std::string NOT_AUTHORIZED;
+		//! La ressource demandée n'a pas été trouvé.
 		extern std::string NOT_FOUND;
+		//! Le destinataire n'est pas connecté.
 		extern std::string REMOTE_NOT_CONNECTED;
 	}
 
@@ -55,14 +96,22 @@ namespace Babel::Protocol
 	public:
 		InvalidPacketException(const std::string &msg) : _msg(msg) {};
 		const char *what() const noexcept override { return this->_msg.c_str(); }
+
+
 	};
 
-	class Packet {
+
+	//! @note Pour plus d'information sur la structuration des packet, voir Protocol
+    //! @warning Si la taille attendu ne correspond pas à la taille donnée dans le header, une erreur BAD_PACKET sera levée.
+    class Packet {
 	private:
+        //! RNG pour générer la clé
 		std::random_device _random;
 
 	public:
+        //! Protocol::Opcode envoyé.
 		unsigned char op;
+        //! données reçu (voir la documentation Protocol::Opcode )
 		std::string data;
 
 		Packet() = default;
@@ -72,7 +121,14 @@ namespace Babel::Protocol
 		Packet &operator=(const std::string &);
 		operator std::string();
 
+        //! @brief Permet de changer un `unsigned` en une chaine de charactère de 4 bytes
+        //! @param value    Valeur à changer
+        //! @return         Une string de longeur 4 réprésentant la value, en "MSBF"
 		static std::string	toByteString(unsigned value);
+
+        //! @brief Permet de changer une une chaine de charactère de 4 bytes en `unsigned`, en "MSBF"
+        //! @param str  Chaine de caractère à changer
+        //! @return     un `unsigned int` réprésentant la chaine de caractère
 		static unsigned		uintFromByteString(
 			const std::string &str);
 	};
--- src/network/Socket.hpp
+++ src/network/Socket.hpp
@@ -19,22 +19,63 @@
 
 namespace Babel
 {
+    //! @brief implémentation custom des sockets.
 	class Socket {
 	public:
 		explicit Socket();
 		Socket(SOCKET sock, bool connected);
+		//! @brief Destructeur
+		//! @details Ferme automatiquement le Socket quand il est détruit
 		~Socket();
+		//! @brief renvoie un `bool` pour savoir si le socket est ouvert.
 		bool			isOpen();
+		//! @brief Ouvre une connection avec un nom d'hote.
+		//! @details Cette méthode récupère l'adresse ip de l'hote, puis appelle this->connect(ip_adress, portno, protocol)
+		//! @param host Nom de l'host
+		//! @param portno Numéro du port
+		//! @param protocol protocol à utiliser
+		//! @throw AlreadyOpenedException
+		//! @throw HostNotFoundException
 		virtual void		connect(const std::string &host, unsigned short portno, int protocol = IPPROTO_TCP);
+        //! @brief Ouvre une connection avec un nom d'hote.
+        //! @param ip Adresse IP du destinataire
+        //! @param portno Numéro du port
+        //! @param protocol protocol à utiliser
+        //! @throw AlreadyOpenedException
+        //! @throw SocketCreationErrorException
+        //! @throw ConnectException
 		virtual void		connect(unsigned int ip, unsigned short portno, int protocol = IPPROTO_TCP);
+		//! @brief Déconnecte le socket.
+		//! @throw NotConnectedException
 		virtual void		disconnect();
+		//! @brief Envoie des données au destinataire
+		//! @throw EOFException
 		virtual void		send(const std::string &);
+		//! @brief Lis les données reçu depuis le destinataire.
+		//! @param size Taille des données à lire, -1 pour lire l'intégralité.
+		//! @param timeout temps maximum d'attente avant
+		//! @return Données lu dans le Socket.
+		//! @throw TimeoutException
+		//! @throw EOFException
 		virtual std::string	read(int size, int timeout = -1);
+		//! @brief Lis toutes les données reçu depuis le destinataire.
+		//! @note Équivaut à Socket.read(-1, timeout).
 		virtual std::string	readUntilEOF(int timeout = -1);
+		//! @brief Renvoie le socket systeme
+		//! @return socket systeme unix ou windows
 		SOCKET			getSocket();
+		//! @brief Lie un socket systeme à l'objet.
+		//! @throw AlreadyOpenedException
 		void			setSocket(SOCKET sock, bool connected);
+		//! @brief Attend que le socket soit pret en lecture.
+		//! @param timeout Temps d'attente avant de lever l'exception TimeoutException
+		//! @note si une valeur négative est envoyé, le Socket attend indéfiniment qu'il soit pret.
+		//! @throw TimeoutException
 		void			waitToBeReady(int timeout);
 
+		//! @brief Renvoie la dernière erreur survenu du Socket.
+		//! @return Chaine de caractère contenant la dernière erreur survenu.
+		//! @warning La valeur est sucsptible de changer si d'autre appel système on était fait entre la dernière erreur du socket et l'appel de cette fonction !
 		static std::string	getLastSocketError();
 
 	protected:
