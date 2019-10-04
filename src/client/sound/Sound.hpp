05b2e8d9ca6b2690b711b86c366c507d883e9caa
Marc-Antoine <marc-antoine.wittling@epitech.eu>
2019-10-04 16:24:02 +0200

    Sample now to 48000

--- src/client/sound/Sound.hpp
+++ src/client/sound/Sound.hpp
@@ -1,31 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
+#include <vector>
 #include "portaudio.h"
 #include <iostream>
 
-#define SAMPLE_RATE  (44100)
+#define SAMPLE_RATE  (48000)
 #define FRAMES_PER_BUFFER (512)
 #define NUM_SECONDS     (5)
 #define NUM_CHANNELS    (2)
 #define DITHER_FLAG     (0)
 #define WRITE_TO_FILE   (0)
-#if 1
-#define PA_SAMPLE_TYPE  paFloat32
-typedef float SAMPLE;
-#define SAMPLE_SILENCE  (0.0f)
-#elif 1
-#define PA_SAMPLE_TYPE  paInt16
-typedef short SAMPLE;
-#define SAMPLE_SILENCE  (0)
-#elif 0
-#define PA_SAMPLE_TYPE  paInt8
-typedef std::vector<unsigned char> SAMPLE;
-#define SAMPLE_SILENCE  (0)
-#else
+
 #define PA_SAMPLE_TYPE  paUInt8
-typedef std::vector<unsigned char> SAMPLE;
-#define SAMPLE_SILENCE  (128)
-#endif
+typedef unsigned char SAMPLE;
+#define SAMPLE_SILENCE  (0)
+
 typedef struct
 {
     int          frameIndex;  /* Index into sample array. */
@@ -52,7 +42,7 @@ class Sound
     Sound();
     ~Sound();
   void playAudio();
-  unsigned char  recordAudio();
+  std::vector <unsigned char>  recordAudio();
   PaError terminate();
   static int recordCallback( const void *inputBuffer, void *outputBuffer,
                                unsigned long framesPerBuffer,
