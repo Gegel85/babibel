908fbe46263e9a3202bc21a1ec9853eb03440d4a
Andgel <andgel.halley@epitech.eu>
2019-10-05 18:02:06 +0200

    Added refuse and accept call

--- src/server/TcpServer.cpp
+++ src/server/TcpServer.cpp
@@ -84,17 +84,23 @@ namespace Babel::Server
 						return TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::BAD_PACKET);
 					return this->_callUser(socket, Network::Protocol::Packet::uint32FromByteString(packet.data));
 				case Network::Protocol::GET_USER_INFOS:
+					return;
 				case Network::Protocol::CALL_ACCEPTED:
+					if (!packet.data.empty())
+						return (TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::BAD_PACKET));
+					return this->_acceptCall(socket);
 				case Network::Protocol::CALL_REFUSED:
+					if (!packet.data.empty())
+						return (TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::BAD_PACKET));
+					return this->_refuseCall(socket);
 				case Network::Protocol::ADD_FRIEND:
 					if (packet.data.size() != 4)
 						return (TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::BAD_PACKET));
-					return (this->_addFriends(socket, Network::Protocol::Packet::uint32FromByteString(packet.data)));
+					return (this->_addFriend(socket, Network::Protocol::Packet::uint32FromByteString( packet.data)));
 				case Network::Protocol::REMOVE_FRIEND:
 					if (packet.data.size() != 4)
 						return (TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::BAD_PACKET));
-						return (this->_removeFriends(socket, Network::Protocol::Packet::uint32FromByteString(packet.data)));
-					return;
+					return (this->_removeFriend(socket, Network::Protocol::Packet::uint32FromByteString(packet.data)));
 				default:
 					return TcpServer::disconnectClient(socket, Network::Protocol::ErrorReason::BAD_OPCODE);
 				}
@@ -112,7 +118,7 @@ namespace Babel::Server
 
 		packet.op = Network::Protocol::HELLO;
 		packet.data = VERSION_STR;
-		std::cout << "Wating for connection..." << std::endl;
+		std::cout << "Waiting for connection..." << std::endl;
 		while (true) {
 			try {
 				this->_socket.waitToBeReady(1);
@@ -131,7 +137,7 @@ namespace Babel::Server
 
 				printf("New client connected\n");
 				sock.send(packet);
-				this->_users.emplace(&sock, Client{0, false, 0, 0});
+				this->_users.emplace(&sock, Client{0, false, 0, 0, {}});
 			} catch (Network::Exceptions::TimeoutException &) {
 			} catch (Network::Exceptions::EOFException &) {}
 		}
@@ -150,7 +156,7 @@ namespace Babel::Server
 		TcpServer::sendPacket(socket, Network::Protocol::OK, data);
 	}
 
-	void TcpServer::_addFriends(Network::Socket &socket,  unsigned int ID)
+	void TcpServer::_addFriend(Network::Socket &socket, unsigned int ID)
 	{
 		auto &user = this->_users.at(&socket);
 
@@ -166,12 +172,11 @@ namespace Babel::Server
 			return(TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::NOT_FOUND));
 	}
 
-	void TcpServer::_removeFriends(Network::Socket &socket, unsigned int ID) {
+	void TcpServer::_removeFriend(Network::Socket &socket, unsigned int ID) {
 		auto &user = this->_users.at(&socket);
 
 		if (!this->_users.at(&socket).connected)
-			return (TcpServer::sendPacket(socket, Network::Protocol::KO,
-										  Network::Protocol::ErrorReason::NOT_CONNECTED));
+			return (TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::NOT_CONNECTED));
 		auto finder = std::find(user.friendList.begin(), user.friendList.end(), ID);
 		if (finder != user.friendList.end()) {
 			user.friendList.erase(finder);
@@ -182,6 +187,88 @@ namespace Babel::Server
 
 	void TcpServer::_callUser(Babel::Network::Socket &socket, unsigned id)
 	{
+		try {
+			auto user = this->_getUser(id);
+			auto &me = this->_users.at(&socket);
+			auto &remote = this->_users.at(user.first);
 
+			if (me.callingUser)
+				return TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::ALREADY_CALLING);
+
+			if (remote.beingCalled)
+				return TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::ALREADY_USED);
+
+			me.callingUser = id;
+			remote.beingCalled = me.userId;
+			this->sendPacket(*user.first, Network::Protocol::CALL, Network::Protocol::Packet::uint32toByteString(me.userId));
+		} catch (Exceptions::UserNotFoundException &) {
+			return TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::REMOTE_NOT_CONNECTED);
+		}
+	}
+
+	void TcpServer::_acceptCall(Babel::Network::Socket &socket)
+	{
+		try {
+			auto &me = this->_users.at(&socket);
+
+			if (!me.beingCalled)
+				return TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::NOT_FOUND);
+
+			auto user = this->_getUser(me.beingCalled);
+			auto &remote = this->_users.at(user.first);
+
+			me.callingUser = 0;
+			remote.beingCalled = 0;
+			this->sendPacket(
+				socket,
+				Network::Protocol::CALL_ACCEPTED,
+				Network::Protocol::Packet::uint32toByteString(socket.getRemoteIp()) +
+				Network::Protocol::Packet::uint16toByteString(DEFAULT_PORT)
+			);
+			this->sendPacket(
+				*user.first,
+				Network::Protocol::CALL_ACCEPTED,
+				Network::Protocol::Packet::uint32toByteString(0) +
+				Network::Protocol::Packet::uint16toByteString(DEFAULT_PORT)
+			);
+		} catch (Exceptions::UserNotFoundException &) {
+			return TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::REMOTE_NOT_CONNECTED);
+		}
+	}
+
+	void TcpServer::_refuseCall(Babel::Network::Socket &socket)
+	{
+		try {
+			auto &me = this->_users.at(&socket);
+
+			if (!me.beingCalled)
+				return TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::NOT_FOUND);
+
+			auto user = this->_getUser(me.beingCalled);
+			auto &remote = this->_users.at(user.first);
+
+			me.callingUser = 0;
+			remote.beingCalled = 0;
+			this->sendPacket(
+				socket,
+				Network::Protocol::OK,
+				""
+			);
+			this->sendPacket(
+				*user.first,
+				Network::Protocol::CALL_REFUSED,
+				""
+			);
+		} catch (Exceptions::UserNotFoundException &) {
+			return TcpServer::sendPacket(socket, Network::Protocol::KO, Network::Protocol::ErrorReason::REMOTE_NOT_CONNECTED);
+		}
+	}
+
+	std::pair<Network::Socket *, std::reference_wrapper<TcpServer::User>> TcpServer::_getUser(unsigned id)
+	{
+		for (auto &user : this->_users)
+			if (user.second.connected && user.second.userId == id)
+				return {user.first, this->_createdUsers[user.second.userId]};
+		throw Exceptions::UserNotFoundException("Cannot find user with id " + std::to_string(id));
 	}
 }
