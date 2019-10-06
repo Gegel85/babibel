beadcf7543ce4cd1811c16bf780ba992f9e2dae6
iElden <mceldenn@gmail.com>
2019-10-06 02:46:39 +0200

    Added Database class

---
+++ src/server/Database.cpp
@@ -0,0 +1,151 @@
+/*
+** EPITECH PROJECT, 2019
+** CPP_babel_2019
+** File description:
+** Database.cpp
+*/
+
+#include <vector>
+#include <iostream>
+#include "Database.hpp"
+
+namespace Babel::Server
+{
+    Database::Database(const std::string &filename, bool debug):
+    _debug(debug)
+    {
+        int rc = sqlite3_open(filename.c_str(), &this->_db);
+
+        if (rc)
+            throw Exceptions::CreationError(std::string("Can't open Database: ") + sqlite3_errmsg(_db));
+        this->_init_tables();
+    }
+
+    Database::~Database()
+    {
+        sqlite3_close(this->_db);
+    }
+
+    std::string Database::get_last_error()
+    {
+        std::string str(this->_last_error);
+        sqlite3_free(this->_last_error);
+        return str;
+    }
+
+    static int av_callback(void *data, int argc, char **argv, char **col_name)
+    {
+        Database *self = reinterpret_cast<Database *>(data);
+        std::vector<DSresponse> result = {};
+        for (int i = 0; i < argc; i++) {
+            self->_last_data.push_back({std::string(col_name[i]), std::string(argv[i])});
+            if (self->_debug)
+                printf("Added in Callback vector: {%s, %s}\n", col_name[i], argv[i]);
+        }
+        return 0;
+    }
+
+
+    std::vector<DSresponse> Database::raw_request(const std::string &request)
+    {
+        this->_last_data.clear();
+        if (this->_debug)
+            std::cout << "Database raw request: " << request << std::endl;
+        int rc = sqlite3_exec(this->_db, request.c_str(), av_callback, this, &this->_last_error);
+        if (rc != SQLITE_OK)
+            throw Exceptions::SQLError(std::string("An SQL Error occured: ") + this->get_last_error());
+        if (this->_debug) {
+            printf("=== LAST DATA ===\n");
+            for (size_t i = 0; i < this->_last_data.size(); i++)
+                printf("{%s, %s}\n", this->_last_data[i].column_name.c_str(), this->_last_data[i].data.c_str());
+            printf("=================\n");
+        }
+        return this->_last_data;
+    }
+
+
+    void Database::_init_tables()
+    {
+        const char *sql = (
+        "CREATE TABLE IF NOT EXISTS Member (\n"
+        "    ID INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+        "    Username CHAR(32) NOT NULL,\n"
+        "    Passwd CHAR(32) NOT NULL\n"
+        ");");
+
+        this->raw_request(sql);
+    }
+
+    void Database::check_forbidden_input(const std::string &str)
+    {
+        std::size_t index = str.find(std::string("'"));
+
+        if (index != std::string::npos)
+            throw Exceptions::ForbiddenInput("A forbidden input has been given, please stop put quote in your input for trying SQL injection.");
+
+    }
+
+    int Database::register_member(const std::string &username, const std::string &passwd)
+    {
+        check_forbidden_input(username);
+        check_forbidden_input(passwd);
+
+        if (this->member_exist(username))
+            throw Exceptions::MemberAlreadyExist("This username is already taken");
+        const std::string sql = std::string("INSERT INTO Member (Username,Passwd)\n") +
+                                "VALUES ('" + username + "', '" + passwd +"');";
+        this->raw_request(sql);
+        return this->check_creds(username, passwd);
+    }
+
+    int Database::check_creds(const std::string &username, const std::string &passwd)
+    {
+        check_forbidden_input(username);
+        check_forbidden_input(passwd);
+
+        const std::string sql = (
+            std::string("SELECT (ID) FROM Member WHERE Username='") +
+            username + "' AND Passwd='" + passwd +"'"
+        );
+        auto data = this->raw_request(sql);
+
+        if (this->_last_data.empty())
+            throw Exceptions::NotFound("Bad credentials given");
+        return std::stoi(data[0].data);
+    }
+
+    bool Database::member_exist(const std::string &username)
+    {
+        try {
+            get_member_by_name(username);
+            return true;
+        }
+        catch (Exceptions::NotFound &_) {
+            return false;
+        }
+    }
+
+    int Database::get_member_by_name(const std::string &username)
+    {
+        check_forbidden_input(username);
+
+        const std::string sql = (
+                std::string("SELECT (ID) FROM Member WHERE Username='") + username + "'");
+        this->raw_request(sql);
+
+        if (this->_last_data.empty())
+            throw Exceptions::NotFound("User with this name was not found");
+        return std::stoi(this->_last_data[0].data);
+    }
+
+    std::string Database::get_user_name(int id)
+    {
+        const std::string sql = (
+                std::string("SELECT (Username) FROM Member WHERE ID = ") + std::to_string(id));
+        this->raw_request(sql);
+
+        if (this->_last_data.empty())
+            throw Exceptions::NotFound("User with this ID was not found");
+        return std::string(this->_last_data[0].data);
+    }
+}
---
+++ src/server/Database.hpp
@@ -0,0 +1,90 @@
+/*
+** EPITECH PROJECT, 2019
+** CPP_babel_2019
+** File description:
+** Database.hpp
+*/
+#ifndef CPP_BABEL_2019_DATABASE_HPP
+#define CPP_BABEL_2019_DATABASE_HPP
+
+#include <sqlite3.h>
+#include <string>
+
+#define DEFAULT_DATABASE_FILENAME std::string("database.db")
+
+namespace Babel::Server {
+
+    struct DSresponse {
+        std::string column_name;
+        std::string data;
+    };
+
+    class Database {
+    private:
+        sqlite3 *_db = nullptr;
+        char *_last_error = nullptr;
+
+        void _init_tables();
+        std::string get_last_error();
+        void check_forbidden_input(const std::string &str);
+
+    public:
+        bool _debug;
+        std::vector<DSresponse> _last_data;
+
+        Database(const std::string &filename = DEFAULT_DATABASE_FILENAME, bool debug=false);
+        ~Database();
+        std::vector<DSresponse> raw_request(const std::string &request);
+        bool member_exist(const std::string &username);
+        int get_member_by_name(const std::string &username);
+        int register_member(const std::string &username, const std::string &passwd);
+        int check_creds(const std::string &username, const std::string &passwd);
+        std::string get_user_name(int id);
+    };
+
+    namespace Exceptions {
+        class SQLBaseException : public std::exception {
+        private:
+            std::string _msg;
+        public:
+            explicit SQLBaseException(const std::string &msg) : _msg(msg)
+            {};
+
+            const char *what() const noexcept override
+            { return this->_msg.c_str(); };
+        };
+
+        class SQLError : public SQLBaseException {
+        public:
+            explicit SQLError(const std::string &msg) : SQLBaseException(msg)
+            {};
+        };
+
+        class CreationError : public SQLBaseException {
+        public:
+            explicit CreationError(const std::string &msg) : SQLBaseException(msg)
+            {};
+        };
+
+        class NotFound : public SQLBaseException {
+        public:
+            explicit NotFound(const std::string &msg) : SQLBaseException(msg)
+            {};
+        };
+
+        class MemberAlreadyExist : public SQLBaseException {
+        public:
+            explicit MemberAlreadyExist(const std::string &msg) : SQLBaseException(msg)
+            {};
+        };
+
+        class ForbiddenInput : public SQLBaseException {
+        public:
+            explicit ForbiddenInput(const std::string &msg) : SQLBaseException(msg)
+            {};
+        };
+    }
+}
+
+
+#endif //CPP_BABEL_2019_DATABASE_HPP
